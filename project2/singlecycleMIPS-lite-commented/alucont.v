70cdc4c1d4b6a259c6a0d3d8d6c3549cf884606f
Arda Öztürk <143508784+ArdaOzturk192@users.noreply.github.com>
2024-05-26 01:01:24 +0300

    alucont fixed

--- project2/singlecycleMIPS-lite-commented/alucont.v
+++ project2/singlecycleMIPS-lite-commented/alucont.v
@@ -9,15 +9,15 @@ if(~(aluop1|aluop0))  gout=3'b010;
 if(aluop0)gout=3'b110;
 if(aluop1)//R-type
 begin
-		if (~(f3|f2|f1|f0))gout=3'b010; 	//function code=0000,ALU control=010 (add)
-		if (f1&f3)gout=3'b111;			//function code=1x1x,ALU control=111 (set on less than)
-		if (f1&~(f3))gout=3'b110;		//function code=0x10,ALU control=110 (sub)
-		if (f2&~(f0))gout=3'b000;		//function code=x1x0,ALU control=000 (and)
-		if (inst_code == 6'b001101) gout = 3'b011; // instruction code 001101 (13) ori (or)
-   	    else if (f2 & f0) begin
+	if (inst_code == 6'b001101) gout = 3'b001;// instruction code 001101 (13) ori (or)
+		else if (~(f3|f2|f1|f0))gout=3'b010; 	//function code=0000,ALU control=010 (add)
+		else if (f1&f3)gout=3'b111;			//function code=1x1x,ALU control=111 (set on less than)
+		else if (f1&~(f3))gout=3'b110;		//function code=0x10,ALU control=110 (sub)
+		else if (f2&~(f0))gout=3'b000;		//function code=x1x0,ALU control=000 (and)
+   	    	else if (f2 & f0) begin
    	     	if (f3 == 0 && f1 == 0) gout = 3'b011;  // function code = 0101, ALU control = 011 (nor) 
-    	    else gout = 3'b001;                    // function code = x1x1, ALU control = 001 (or)
-  	  end
+    	    	else gout = 3'b001;                    // function code = x1x1, ALU control = 001 (or)
+  	  	end
 	end
 end
 endmodule
